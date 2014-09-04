b31f783af7fce863ffc77c80264e657c1b282c9e
Josh Wolfe <thejoshwolfe@gmail.com>
2014-09-03 18:23:53 -0700

    trash 2

--- index.js
+++ index.js
@@ -11,60 +11,78 @@ function open(path, callback) {
   if (callback == null) callback = defaultCallback;
   fs.open(path, "w", function(err, fd) {
     if (err) return callback(err);
-    callback(null, new ZipFile(fd, {autoClose: true}));
+    var outputStream = fs.createWriteStream(path, {fd: fd});
+    callback(null, new ZipFile(outputStream, {autoClose: true}));
   });
 }
 
-function ZipFile(fd, options) {
-  this.fd = fd;
+function ZipFile(outputStream, options) {
+  this.outputStream = outputStream;
   options = options || {autoClose: false};
   this.autoClose = !!options.autoClose;
   this.entries = [];
   this.centralDirectoryBuffers = [];
-  this.cursor = 0;
+  this.outputStreamCursor = 0;
 }
-ZipFile.prototype.addFile = function(realPath, options, callback) {
+ZipFile.prototype.addFile = function(realPath, options) {
   var self = this;
-  if (callback == null) callback = defaultCallback;
-
-  var metadataPath = options.metadataPath;
-  if (metadataPath == null) {
-    if (options.archiveRootPath == null) throw new Error("one of metadataPath or archiveRootPath is required");
-    metadataPath = path.relative(options.archiveRootPath, realPath);
-  }
-  if (metadataPath.indexOf("\\") !== -1) throw new Error("invalid characters in path: " + metadataPath);
-  if (/^[a-zA-Z]:/.test(metadataPath) || /^\//.test(metadataPath)) throw new Error("absolute path: " + metadataPath);
-  if (metadataPath.split("/").indexOf("..") !== -1) throw new Error("invalid relative path: " + metadataPath);
+  var metadataPath = getMetadataPath(options, realPath);
 
   var entry = new Entry(metadataPath, options);
   self.entries.push(entry);
   fs.open(realPath, "r", function(err, fd) {
-    if (err) return callback(err);
+    if (err) return self.emit("error", err);
     fs.fstat(fd, function(err, stats) {
-      if (err) return callback(err);
-      if (!stats.isFile()) return callback(new Error("not a file: " + realPath));
+      if (err) return self.emit("error", err);
+      if (!stats.isFile()) return self.emit("error", new Error("not a file: " + realPath));
       entry.setLastModDate(stats.mtime);
       entry.setPumper(function(sink) {
         var readStream = fs.createReadStream(null, {fd: fd});
         readStream.on("error", function(err) {
-          callback(err);
-        });
-        readStream.on("data", function(data) {
-          sink.write(data);
+          self.emit("error", err);
         });
         readStream.on("end", function() {
-          sink.end();
+          entry.dataWritten = true;
+          pumpEntries(self);
+        });
+        readStream.pipe(self.outputStream, {end: false});
+        readStream.on("data", function(data) {
+          self.outputStreamCursor += data.length;
         });
       });
       pumpEntries(self);
     });
   });
 };
-ZipFile.prototype.addBuffer = function(buffer, options, callback) {
+ZipFile.prototype.addBuffer = function(buffer, options) {
   var self = this;
+  var metadataPath = getMetadataPath(options);
+  var entry = new Entry(metadataPath, options);
+  if (options.mtime == null) throw new Error("missing mtime");
+  entry.setLastModDate(options.mtime);
+  self.entries.push(entry);
+  entry.setPumper(function(sink) {
+    sink.write(buffer);
+    sink.end();
+    pumpEntries(self);
+  });
+  pumpEntries(self);
 };
 
 function pumpEntries(self) {
+}
+
+function getMetadataPath(options, realPath) {
+  var metadataPath = options.metadataPath;
+  if (metadataPath == null) {
+    if (realPath == null) throw new Error("missing metadataPath");
+    if (options.archiveRootPath == null) throw new Error("one of metadataPath or archiveRootPath is required");
+    metadataPath = path.relative(options.archiveRootPath, realPath);
+  }
+  if (metadataPath.indexOf("\\") !== -1) throw new Error("invalid characters in path: " + metadataPath);
+  if (/^[a-zA-Z]:/.test(metadataPath) || /^\//.test(metadataPath)) throw new Error("absolute path: " + metadataPath);
+  if (metadataPath.split("/").indexOf("..") !== -1) throw new Error("invalid relative path: " + metadataPath);
+  return metadataPath;
 }
 
 function Entry(metadataPath, options) {
