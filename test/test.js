330168b94da77866a9604035fda4e99af476bff2
Josh Wolfe <thejoshwolfe@gmail.com>
2014-09-14 13:54:17 -0700

    unicode test. ... 'test'

--- test/test.js
+++ test/test.js
@@ -4,6 +4,6 @@ var path = require("path");
 yazl.open(path.join(__dirname, "dump.zip"), function(err, zipfile) {
   if (err) throw err;
   // it's me!
-  zipfile.addFile(__filename, "asdf.txt");
+  zipfile.addFile(__filename, "unicōde.txt");
   zipfile.end();
 });
