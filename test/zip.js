50941a7edffaf600d982ffa8089460410aed928c
Josh Wolfe <thejoshwolfe@gmail.com>
2015-03-01 18:21:39 -0700

    adding features to test/zip.js to reproduce #9.
    $ node --stack-size=100 --stack-trace-limit=200 test/zip.js index.js --buffer $(for i in {1..1000}; do echo index.js; done) -o /dev/null

--- test/zip.js
+++ test/zip.js
@@ -1,27 +1,49 @@
 var usage = "node " + __filename.replace(/.*[\/\\]/, "") + " " +
-            "[FILE | --compress | --no-compress]... -o OUTPUT.zip";
+            "[FILE | --compress | --no-compress | --buffer | --no-buffer]... -o OUTPUT.zip" + "\n" +
+            "\n" +
+            "all arguments and switches are processed in order. for example:" + "\n" +
+            "  node zip.js --compress a.txt --no-compress b.txt -o out.zip" + "\n" +
+            "would result in compression for a.txt, but not for b.txt.";
 var yazl = require("../");
 var fs = require("fs");
 
 var zipfile = new yazl.ZipFile();
 var options = {compress: false};
+var use_buffer = false;
 
 var args = process.argv.slice(2);
-if (Math.max(args.indexOf("-h"), args.indexOf("--help")) !== -1) throw new Error("usage: " + usage);
-var outputFileIndex = args.indexOf("-o");
-if (outputFileIndex === -1) throw new Error("missing -o");
-zipfile.outputStream.pipe(fs.createWriteStream(args[outputFileIndex + 1]));
-args.splice(outputFileIndex, 2);
+if (Math.max(args.indexOf("-h"), args.indexOf("--help")) !== -1) {
+  console.log("usage: " + usage);
+  process.exit(1);
+}
+// this one's important
+if (args.indexOf("-o") === -1) throw new Error("missing -o");
+if (args.indexOf("-o") + 1 >= args.length) throw new Error("missing argument after -o");
+
+var its_the_dash_o = false;
 args.forEach(function(arg) {
-  if (/--compress/.test(arg)) {
+  if (its_the_dash_o) {
+    its_the_dash_o = false;
+    zipfile.outputStream.pipe(fs.createWriteStream(arg));
+  } else if (arg === "--compress") {
     options.compress = true;
-  } else if (/--no-compress/.test(arg)) {
+  } else if (arg === "--no-compress") {
     options.compress = false;
+  } else if (arg === "--buffer") {
+    use_buffer = true;
+  } else if (arg === "--no-buffer") {
+    use_buffer = false;
+  } else if (arg === "-o") {
+    its_the_dash_o = true;
   } else {
     // file thing
     var stats = fs.statSync(arg);
     if (stats.isFile()) {
-      zipfile.addFile(arg, arg, options);
+      if (use_buffer) {
+        zipfile.addBuffer(fs.readFileSync(arg), arg, options);
+      } else {
+        zipfile.addFile(arg, arg, options);
+      }
     } else if (stats.isDirectory()) {
       zipfile.addEmptyDirectory(arg);
     } else {
