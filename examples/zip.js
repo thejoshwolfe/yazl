c9fbaa97f285850972cd93f100b5b93ade671f4b
Josh Wolfe <thejoshwolfe@gmail.com>
2016-06-26 11:40:42 -0700

    examples/zip.js: add --verbose and --stream options and print final size prediction

--- examples/zip.js
+++ examples/zip.js
@@ -1,6 +1,6 @@
 var usage =
   "node " + __filename.replace(/.*[\/\\]/, "") + " " +
-  "[FILE | --compress | --no-compress | --buffer | --no-buffer | --zip64={true|false|auto}]... -o OUTPUT.zip" + "\n" +
+  "[FILE | --[no-]compress | {--file|--buffer|--stream} | --[no-]zip64 | --[no-]verbose]... -o OUTPUT.zip" + "\n" +
   "\n" +
   "all arguments and switches are processed in order. for example:" + "\n" +
   "  node zip.js --compress a.txt --no-compress b.txt -o out.zip" + "\n" +
@@ -10,7 +10,8 @@ var fs = require("fs");
 
 var zipfile = new yazl.ZipFile();
 var options = {compress: false, forceZip64Format: false};
-var use_buffer = false;
+var addStrategy = "addFile";
+var verbose = false;
 
 var args = process.argv.slice(2);
 if (Math.max(args.indexOf("-h"), args.indexOf("--help")) !== -1) {
@@ -31,16 +32,20 @@ args.forEach(function(arg) {
     options.compress = true;
   } else if (arg === "--no-compress") {
     options.compress = false;
+  } else if (arg === "--file") {
+    addStrategy = "addFile";
   } else if (arg === "--buffer") {
-    use_buffer = true;
-  } else if (arg === "--zip64=true") {
+    addStrategy = "addBuffer";
+  } else if (arg === "--stream") {
+    addStrategy = "addReadStream";
+  } else if (arg === "--no-verbose") {
+    verbose = false;
+  } else if (arg === "--verbose") {
+    verbose = true;
+  } else if (arg === "--zip64") {
     options.forceZip64Format = true;
-  } else if (arg === "--zip64=false") {
+  } else if (arg === "--no-zip64") {
     options.forceZip64Format = false;
-  } else if (arg === "--zip64=auto") {
-    options.forceZip64Format = "auto";
-  } else if (arg === "--no-buffer") {
-    use_buffer = false;
   } else if (arg === "-o") {
     its_the_dash_o = true;
   } else if (arg === "-") {
@@ -49,16 +54,39 @@ args.forEach(function(arg) {
     // file thing
     var stats = fs.statSync(arg);
     if (stats.isFile()) {
-      if (use_buffer) {
-        zipfile.addBuffer(fs.readFileSync(arg), arg, options);
-      } else {
-        zipfile.addFile(arg, arg, options);
+      switch (addStrategy) {
+        case "addFile":
+          if (verbose) console.log("addFile(" +
+                                   JSON.stringify(arg) + ", " +
+                                   JSON.stringify(arg) + ", " +
+                                   JSON.stringify(options) + ");");
+          zipfile.addFile(arg, arg, options);
+          break;
+        case "addBuffer":
+          if (verbose) console.log("addBuffer(fs.readFileSync(" +
+                                   JSON.stringify(arg) + "), " +
+                                   JSON.stringify(arg) + ", " +
+                                   JSON.stringify(options) + ");");
+          zipfile.addBuffer(fs.readFileSync(arg), arg, options);
+          break;
+        case "addReadStream":
+          if (verbose) console.log("addReadStream(fs.createReadStream(" +
+                                   JSON.stringify(arg) + "), " +
+                                   JSON.stringify(arg) + ", " +
+                                   JSON.stringify(options) + ");");
+          zipfile.addReadStream(fs.createReadStream(arg), arg, options);
+          break;
+        default: throw new Error();
       }
     } else if (stats.isDirectory()) {
+      if (verbose) console.log("addEmptyDirectory(" +
+                               JSON.stringify(arg) + ", ");
       zipfile.addEmptyDirectory(arg);
     } else {
       throw new Error("what is this: " + arg);
     }
   }
 });
-zipfile.end({forceZip64Format: options.forceZip64Format});
+zipfile.end({forceZip64Format: options.forceZip64Format}, function(finalSize) {
+  console.log("finalSize prediction: " + finalSize);
+});
