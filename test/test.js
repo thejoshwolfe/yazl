6a8c2fc8d27d26f8c1ec1593af26a5b8dfc10ee1
Josh Wolfe <thejoshwolfe@gmail.com>
2018-11-15 22:19:34 -0500

    more tests for comments

--- test/test.js
+++ test/test.js
@@ -127,50 +127,63 @@ var BufferList = require("bl");
   });
 })();
 
+var weirdChars = '\u0000☺☻♥♦♣♠•◘○◙♂♀♪♫☼►◄↕‼¶§▬↨↑↓→←∟↔▲▼⌂ÇüéâäàåçêëèïîìÄÅÉæÆôöòûùÿÖÜ¢£¥₧ƒáíóúñÑªº¿⌐¬½¼¡«»░▒▓│┤╡╢╖╕╣║╗╝╜╛┐└┴┬├─┼╞╟╚╔╩╦╠═╬╧╨╤╥╙╘╒╓╫╪┘┌█▄▌▐▀αßΓπΣσµτΦΘΩδ∞φε∩≡±≥≤⌠⌡÷≈°∙·√ⁿ²■ ';
 (function() {
-  var zipfile = new yazl.ZipFile();
-  var comment = "Hello World";
-  zipfile.end({
-    comment: comment
-  }, function(finalSize) {
-    if (finalSize === -1) throw new Error("finalSize should be known");
-    zipfile.outputStream.pipe(new BufferList(function(err, data) {
-      if (err) throw err;
-      if (data.length !== finalSize) throw new Error("finalSize prediction is wrong. " + finalSize + " !== " + data.length);
-      yauzl.fromBuffer(data, function(err, zipfile) {
+  var testCases = [
+    ["Hello World", "Hello World"],
+    [Buffer.from("Hello"), "Hello"],
+    [weirdChars, weirdChars],
+  ];
+  testCases.forEach(function(testCase, i) {
+    var zipfile = new yazl.ZipFile();
+    zipfile.end({
+      comment: testCase[0]
+    }, function(finalSize) {
+      if (finalSize === -1) throw new Error("finalSize should be known");
+      zipfile.outputStream.pipe(new BufferList(function(err, data) {
         if (err) throw err;
-        if (zipfile.comment !== comment) {
-          throw new Error("fileComment didn't match: " + zipfile.fileComment + ", expected: " + fileComment);
-        }
-        console.log("comment: pass");
-      });
-    }));
+        if (data.length !== finalSize) throw new Error("finalSize prediction is wrong. " + finalSize + " !== " + data.length);
+        yauzl.fromBuffer(data, function(err, zipfile) {
+          if (err) throw err;
+          if (zipfile.comment !== testCase[1]) {
+            throw new Error("comment is wrong. " + JSON.stringify(zipfile.comment) + " !== " + JSON.stringify(testCase[1]));
+          }
+          console.log("comment(" + i + "): pass");
+        });
+      }));
+    });
   });
 })();
 
 (function() {
-  var fileComment = "Hello World";
-  var zipfile = new yazl.ZipFile();
-  // all options parameters are optional
-  zipfile.addBuffer(Buffer.from("hello"), "hello.txt", {compress: false, fileComment: fileComment});
-  zipfile.end(function(finalSize) {
-    if (finalSize === -1) throw new Error("finalSize should be known");
-    zipfile.outputStream.pipe(new BufferList(function(err, data) {
-      if (err) throw err;
-      if (data.length !== finalSize) throw new Error("finalSize prediction is wrong. " + finalSize + " !== " + data.length);
-      yauzl.fromBuffer(data, function(err, zipfile) {
+  var testCases = [
+    ["Hello World!", "Hello World!"],
+    [Buffer.from("Hello!"), "Hello!"],
+    [weirdChars, weirdChars],
+  ];
+  testCases.forEach(function(testCase, i) {
+    var zipfile = new yazl.ZipFile();
+    // all options parameters are optional
+    zipfile.addBuffer(Buffer.from("hello"), "hello.txt", {compress: false, fileComment: testCase[0]});
+    zipfile.end(function(finalSize) {
+      if (finalSize === -1) throw new Error("finalSize should be known");
+      zipfile.outputStream.pipe(new BufferList(function(err, data) {
         if (err) throw err;
-        var entryNames = ["hello.txt"];
-        zipfile.on("entry", function(entry) {
-          var expectedName = entryNames.shift();
-          if (entry.fileComment !== fileComment) {
-            throw new Error("unexpected entry fileComment: " + entry.fileComment + ", expected: " + fileComment);
-          }
+        if (data.length !== finalSize) throw new Error("finalSize prediction is wrong. " + finalSize + " !== " + data.length);
+        yauzl.fromBuffer(data, function(err, zipfile) {
+          if (err) throw err;
+          var entryNames = ["hello.txt"];
+          zipfile.on("entry", function(entry) {
+            var expectedName = entryNames.shift();
+            if (entry.fileComment !== testCase[1]) {
+              throw new Error("fileComment is wrong. " + JSON.stringify(entry.fileComment) + " !== " + JSON.stringify(testCase[1]));
+            }
+          });
+          zipfile.on("end", function() {
+            if (entryNames.length === 0) console.log("fileComment(" + i + "): pass");
+          });
         });
-        zipfile.on("end", function() {
-          if (entryNames.length === 0) console.log("fileComment: pass");
-        });
-      });
-    }));
+      }));
+    });
   });
 })();
